d3034a2b71e0bd5991843158bf9aa5d0da261091
capic <vcapic@gmail.com>
2018-07-29 17:08:33 +0200

    update pyyaml

--- src/main.py
+++ src/main.py
@@ -54,6 +54,7 @@ from actions import hue_control
 from actions import vlcplayer
 from actions import spotify_playlist_select
 from actions import configuration
+from actions import get_best_accurate_command
 import snowboydecoder
 import sys
 import signal
